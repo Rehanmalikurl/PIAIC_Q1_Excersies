0feaf310e68e00b9189a70167ad0e0c2aa677b56
Rehanmalikurl <139068855+Rehanmalikurl@users.noreply.github.com>
2023-09-08 22:34:46 +0500

    Question#2

---
+++ ex1/index.ts
@@ -0,0 +1,5 @@
+//Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+
+let personName: string ="Rehan Malik"
+let message: string = `Hi ${personName} , How are you doing?`
+console.log(message);
