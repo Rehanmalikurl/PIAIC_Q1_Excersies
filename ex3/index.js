e15c3976cff602f859941fe3724537e539250045
Rehanmalikurl <139068855+Rehanmalikurl@users.noreply.github.com>
2023-09-09 00:21:43 +0500

    question#3

---
+++ ex3/index.js
@@ -0,0 +1,16 @@
+"use strict";
+// Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+// step 1 = Lowercase
+let personName = "muhammad rehan malik";
+let lowercase = personName.toLowerCase();
+console.log(lowercase);
+// step 2 = Uppercase
+let Uppercase = personName.toUpperCase();
+console.log(Uppercase);
+// step 3 = titelcase with Array, for loop and their functions
+let words = personName.split(" ");
+let titelcaseName = "";
+for (let i = 0; i < words.length; i++) {
+    titelcaseName += words[i].charAt(0).toUpperCase() + words[i].slice(1).toLowerCase() + " ";
+}
+console.log(titelcaseName);
