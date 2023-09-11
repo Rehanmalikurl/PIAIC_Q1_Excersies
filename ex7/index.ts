deef46d9d53a9b73a1e5b6df5abcf826d71cab20
Rehanmalikurl <139068855+Rehanmalikurl@users.noreply.github.com>
2023-09-11 09:58:42 +0500

    Question#7

---
+++ ex7/index.ts
@@ -0,0 +1,21 @@
+// Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+// add:
+
+const addition = 4+4;
+console.log("result: " + addition)
+
+// sub:
+
+const subtraction = 12-4
+console.log("result: " + subtraction);
+
+// multiply:
+
+const multiplication = 2*4
+console.log("result: " + multiplication )
+
+// division:
+
+const division = 16/2
+console.log( "result: " + division);
+
