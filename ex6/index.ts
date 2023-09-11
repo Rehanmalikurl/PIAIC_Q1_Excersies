64fa1307821694349bd83cee2788ebbfea6e7b30
Rehanmalikurl <139068855+Rehanmalikurl@users.noreply.github.com>
2023-09-11 09:23:58 +0500

    Question#6

---
+++ ex6/index.ts
@@ -0,0 +1,10 @@
+// Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.
+
+// step 1 with spacies
+
+let personName: string= "\t            Rehan Malik              \n "
+ console.log(personName );
+
+let strippedName: string= personName.trim();
+
+console.log("bhai;",strippedName , "done")
