30aebf9b357279b121047ac3116b755d57a27772
Rehanmalikurl <139068855+Rehanmalikurl@users.noreply.github.com>
2023-09-11 09:59:14 +0500

    Question#8

---
+++ ex8/index.js
@@ -0,0 +1,13 @@
+"use strict";
+//You should create four lines that look like this:
+//console.log(5 + 3)
+// Your output should simply be four lines with the number 8 appearing once on each line.
+console.log("_".repeat(50));
+console.log(4 + 4);
+console.log("_".repeat(50));
+console.log(12 - 4);
+console.log("_".repeat(50));
+console.log(2 * 4);
+console.log("_".repeat(50));
+console.log(16 / 2);
+console.log("_".repeat(50));
