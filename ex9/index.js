53429b7c3822ff990fcfeadf0a11faee9029a1ed
Rehanmalikurl <139068855+Rehanmalikurl@users.noreply.github.com>
2023-09-12 09:17:12 +0500

    Question#9

---
+++ ex9/index.js
@@ -0,0 +1,11 @@
+"use strict";
+// Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+// let personName: string = " Rehan Malik ";
+// const personAge: Number = 23;
+// var isStudent: boolean = true
+// personName = 23;
+// personAge = "23"
+// isStudent = yes 
+let favoriteNumber = 3;
+let message = `Hi, my favorite  number is ${favoriteNumber}.`;
+console.log(message);
