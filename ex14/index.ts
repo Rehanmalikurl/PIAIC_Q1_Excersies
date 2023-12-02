360caee99d25aa4ab346b215e9dc69617f0f3512
Rehanmalikurl <139068855+Rehanmalikurl@users.noreply.github.com>
2023-12-02 14:35:31 +0500

    Question#14

---
+++ ex14/index.ts
@@ -0,0 +1,17 @@
+// Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner
+
+let gusetlist:Array<string> = [
+    "AQSA",
+    "NOMI",
+    "BHai",
+    "AMI",
+    "MANI",
+    "ANSA"
+
+]
+
+gusetlist.forEach((gusetlist)=>
+{
+    console.log(`Dear, ${gusetlist} Im inviteibg you for the Dinner tonight at my Home.`)
+}
+)
